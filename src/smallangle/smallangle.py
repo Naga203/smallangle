74c9bdea12c3410a0d2c543852942121ef3f5be4
Naga203 <135603043+Naga203@users.noreply.github.com>
2023-11-17 15:37:16 +0100

    smallangle now has docstrings and comments

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,23 +3,31 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+# we will create a group of commands
 @click.group()
 def cmd_group():
     pass
 
+# add subcommand to calculate sin
 @cmd_group.command()
 @click.option(
     "--number",
     "-n",
     default=10,
-    help="Number of x values between 0 and 2pi.",
+    help="Number of x values evenly spaced between 0 and 2pi.",
     show_default=True)
 def sin(number):
+    """print sin of *number* values between 0 and 2pi
+
+    Args:
+        number (int): 0 and 2pi is evenly divided into *number* values inbetween
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
+# add subcommand to calculate tan
 @cmd_group.command()
 @click.option(
     "-number",
@@ -28,6 +36,11 @@ def sin(number):
     help="Number of x values between 0 and 2pi.",
     show_default=True)
 def tan(number):
+    """print tan of *number* values between 0 and 2pi
+
+    Args:
+        number (int): 0 and 2pi is evenly divided into *number* values inbetween
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
@@ -35,3 +48,8 @@ def tan(number):
 
 if __name__ == "__main__":
     cmd_group()
+
+# information on the subcommands and their functions
+# obtain with smallangle.py (subcommand) --help
+help(sin)
+help(tan)
